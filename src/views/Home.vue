03456a47eddfa39cc10e94e117aba0f0c99553e0
gaojia <sofia7452@163.com>
2021-01-14 21:02:12 +0800

    aside css 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template lang="">
   <div>
-   <Topnav/>
-    <div class="banner">
-      <h1>轮子UI</h1>
-      <h2>一个好用的UI框架</h2>
-      <p class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
-      </p>
+  <Topnav/>
+  <div class="banner">
+    <h1>轮子UI</h1>
+    <h2>一个好用的UI框架</h2>
+    <p class="actions">
+    <a href="">GitHub</a>
+    <router-link to='/doc'>开始</router-link>
+    </p>
     </div>
-  </div>
+    </div>
 </template>
 <script lang="ts">
   import Topnav from '../components/Topnav.vue'
@@ -17,3 +17,26 @@
     components:{Topnav}
   }
 </script>
+<style lang="scss" scoped>
+    .banner {
+        padding: 100px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        background: lightgreen;
+        > .actions{
+            padding: 8px 0;
+            a {
+                margin:0 8px;
+                background: #fff;
+                display: inline-block;
+                $h: 28px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
+                padding: 0 8px;
+            }
+        }
+    }
+</style>
