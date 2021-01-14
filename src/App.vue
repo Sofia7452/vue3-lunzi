bdc92924d34f33bf4a22b025b78c68e88f54415d
gaojia <sofia7452@163.com>
2021-01-14 21:59:24 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
+import { provide, ref } from "vue";
 export default {
   name: "App",
+  setup() {
+    // provide使得后代都可以用，后代通过inject获取
+    const menuVisible = ref(false);
+    provide("xxx", menuVisible);
+  },
 };
 </script>
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,30 +1,44 @@
 <template>
-    <div class='topnav'>
-        <div class="logo">LOGO</div>
-        <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-        </ul>
-    </div>
+  <div class="topnav">
+    <div class="logo" @click="toggleMenu">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
 </template>
+<script lang='ts'>
+import { inject, Ref } from "vue";
+export default {
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("xxx");
+    console.log("topnav 获取的 MenuVisible 为：" + menuVisible.value);
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    // return { toggleMenu };这里return 上面的模版才能用
+    return { toggleMenu };
+  },
+};
+</script>
 <style lang="scss" scoped>
-    .topnav {
-        background: pink;
-        display: flex;
-        padding: 16px;
-        position:relative;
-        z-index:10;
-        > .logo {
-            max-width: 6em;
-            margin-right: auto;
-        }
-        > .menu {
-            display: flex;
-            white-space: nowrap;
-            flex-wrap: nowrap;
-            > li {
-                margin: 0 1em;
-            }
-        }
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  position: relative;
+  z-index: 10;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
     }
+  }
+}
 </style>
