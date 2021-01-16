e0c5d68f978640a0ed7117ec367033099e117d1b
gaojia <sofia7452@163.com>
2021-01-16 11:32:29 +0800

    基本完成官网框架

--- src/router.ts
+++ src/router.ts
@@ -4,13 +4,21 @@ import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import { createWebHashHistory, createRouter } from 'vue-router';
 import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
+import DiologDemo from './components/DiologDemo.vue'
+import TabsDemo from './components/TabsDemo.vue'
+import DocDemo from './components/DocDemo.vue'
 const history = createWebHashHistory();
 export const router = createRouter({
   history, routes: [
     { path: '/', component: Home },
     {
       path: '/doc', component: Doc, children: [
-        { path: 'switch', component: SwitchDemo }
+        { path: '', component: DocDemo },
+        { path: 'switch', component: SwitchDemo },
+        { path: 'button', component: ButtonDemo },
+        { path: 'diolog', component: DiologDemo },
+        { path: 'tabs', component: TabsDemo }
       ]
     }
   ]
