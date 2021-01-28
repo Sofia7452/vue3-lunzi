4e9225e4e44758e006bda5c5858a7ad066ce8312
gaojia <sofia7452@163.com>
2021-01-28 12:48:25 +0800

    ke

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <Switch />
+    <!--如果value没有加冒号，这里就是字符串而不是变量-->
+    <Switch :value="y" @input="y = $event" />
   </div>
 </template>
-<script>
+<script lang="ts">
 import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
 export default {
   components: { Switch },
+  setup() {
+    const y = ref(false);
+    return { y };
+  },
 };
 </script>
